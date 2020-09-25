2febcf3f1d7a3f5769905c596c388c98e16980a0
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-09-25 11:20:39 +0700

    aaaa

--- pages/index.vue
+++ pages/index.vue
@@ -36,34 +36,13 @@
               <td class="px-4 py-2 border">
                 <p class="w-full mt-1">{{ row.hour }}</p>
               </td>
-              <td class="px-4 py-2 border">
+              <td
+                v-for="(day, indexDay) in row.days"
+                :key="indexDay"
+                class="px-4 py-2 border"
+              >
                 <p class="block w-full mt-1">
-                  {{ row.days.monday ? 'Ya' : 'Tidak' }}
-                </p>
-              </td>
-              <td class="px-4 py-2 border">
-                <p class="block w-full mt-1">
-                  {{ row.days.tuesday ? 'Ya' : 'Tidak' }}
-                </p>
-              </td>
-              <td class="px-4 py-2 border">
-                <p class="block w-full mt-1">
-                  {{ row.days.wednesday ? 'Ya' : 'Tidak' }}
-                </p>
-              </td>
-              <td class="px-4 py-2 border">
-                <p class="block w-full mt-1">
-                  {{ row.days.thursday ? 'Ya' : 'Tidak' }}
-                </p>
-              </td>
-              <td class="px-4 py-2 border">
-                <p class="block w-full mt-1">
-                  {{ row.days.friday ? 'Ya' : 'Tidak' }}
-                </p>
-              </td>
-              <td class="px-4 py-2 border">
-                <p class="block w-full mt-1">
-                  {{ row.days.saturday ? 'Ya' : 'Tidak' }}
+                  {{ day ? 'Ya' : 'Tidak' }}
                 </p>
               </td>
             </tr>
@@ -184,6 +163,7 @@
       <div class="flex items-start justify-between mt-4">
         <div class="flex flex-col justify-start">
           <p>Total jam: {{ currentInputScheduleTimeLength() }}</p>
+          <p>Total instruktur: {{ instructorCount() }}</p>
           <p>Maksimal jam: 60/61/62</p>
         </div>
         <div class="flex justify-end space-x-4">
@@ -310,8 +290,8 @@
 </template>
 
 <script>
-import faker from 'faker'
 /* eslint-disable no-unused-vars */
+import faker from 'faker'
 
 export default {
   data() {
@@ -331,14 +311,7 @@ export default {
         id: 0,
         name: '',
         hour: 1,
-        days: {
-          monday: false,
-          tuesday: false,
-          wednesday: false,
-          thursday: false,
-          friday: false,
-          saturday: false,
-        },
+        days: [false, false, false, false, false, false],
       },
       newSchedule: {},
       inputSchedules: [],
@@ -353,36 +326,31 @@ export default {
       id: 0,
       name: '',
       hour: 1,
-      days: {
-        monday: false,
-        tuesday: false,
-        wednesday: false,
-        thursday: false,
-        friday: false,
-        saturday: false,
-      },
+      days: [false, false, false, false, false, false],
     }
   },
   methods: {
     addRow() {
       const copyNewSchedule = { ...this.newSchedule }
-      let lastId = this.inputSchedules.length + 1
-      copyNewSchedule.id = lastId++
-      copyNewSchedule.hour = +copyNewSchedule.hour
-      this.newSchedule = {
-        id: 0,
-        name: '',
-        hour: 1,
-        days: {
-          monday: false,
-          tuesday: false,
-          wednesday: false,
-          thursday: false,
-          friday: false,
-          saturday: false,
-        },
+      // cek jamnya
+      if (
+        this.currentInputScheduleTimeLength() +
+          this.scheduleHourLength(copyNewSchedule) >
+        62
+      ) {
+        alert('Jumlah jam atau hari terlalu banyak, mohon dikurangi')
+      } else {
+        let lastId = this.inputSchedules.length + 1
+        copyNewSchedule.id = lastId++
+        copyNewSchedule.hour = +copyNewSchedule.hour
+        this.newSchedule = {
+          id: 0,
+          name: '',
+          hour: 1,
+          days: [false, false, false, false, false, false],
+        }
+        this.inputSchedules.push(copyNewSchedule)
       }
-      this.inputSchedules.push(copyNewSchedule)
     },
     populate() {
       this.inputSchedules = []
@@ -392,14 +360,14 @@ export default {
           id: index,
           name: faker.name.findName(),
           hour: this.randomHour(),
-          days: {
-            monday: this.randomBoolean(),
-            tuesday: this.randomBoolean(),
-            wednesday: this.randomBoolean(),
-            thursday: this.randomBoolean(),
-            friday: this.randomBoolean(),
-            saturday: this.randomBoolean(),
-          },
+          days: [
+            this.randomBoolean(),
+            this.randomBoolean(),
+            this.randomBoolean(),
+            this.randomBoolean(),
+            this.randomBoolean(),
+            this.randomBoolean(),
+          ],
         })
         index++
       }
@@ -412,8 +380,18 @@ export default {
     },
     currentInputScheduleTimeLength() {
       let count = 0
-      this.inputSchedules.map((e) => (count += e.hour))
+      this.inputSchedules.map((schedule) => {
+        const hourPerPerson =
+          schedule.hour * schedule.days.filter((day) => day === true).length
+        count += hourPerPerson
+      })
       return count
+    },
+    scheduleHourLength(schedule) {
+      return schedule.hour * schedule.days.filter((day) => day === true).length
+    },
+    instructorCount() {
+      return this.inputSchedules.length
     },
     nextWeekHourCount() {
       let count = 0
@@ -431,50 +409,100 @@ export default {
       return count
     },
     makeSchedule() {
+      const currentSchedules = [...this.inputSchedules]
+      const nextWeekSchedulesPerPerson = []
+      let totalTeachingHours = 0
+
+      currentSchedules.forEach((schedule) => {
+        const dailySchedules = []
+        schedule.days.forEach((day) => {
+          const hourlySchedule = []
+          if (day) {
+            for (let index = 1; index <= schedule.hour; index++) {
+              hourlySchedule.push(schedule)
+              totalTeachingHours++
+            }
+          }
+          dailySchedules.push(hourlySchedule)
+        })
+        nextWeekSchedulesPerPerson.push(dailySchedules)
+      })
+
+      console.log(nextWeekSchedulesPerPerson)
+      console.log(totalTeachingHours)
+
+      const nextWeekSchedulesPerDay = []
+
+      for (let index = 0; index < 6; index++) {
+        const dailySchedules = []
+        nextWeekSchedulesPerPerson.forEach(
+          (schedulePerPerson, indexPerPerson) => {
+            if (indexPerPerson === index) {
+              // const apa = []
+              // console.log(schedulePerPerson)
+              schedulePerPerson.forEach((schedulePerDay, i) => {
+                if (schedulePerDay.length > 1) {
+                  // console.log(schedulePerDay)
+                  dailySchedules.push(schedulePerDay)
+                }
+              })
+              // dailySchedules.push(apa)
+            }
+          }
+        )
+
+        nextWeekSchedulesPerDay.push(dailySchedules)
+      }
+
+      console.log(nextWeekSchedulesPerDay)
+      // let what = 0
+      // nextWeekSchedules.forEach((schedule) => {
+      //   schedule.forEach((day) => {})
+      // })
       // pilih random array
       // sampai ketemu >= 48 jam
       // yang sudah 48, keluarkan dari array
 
-      const maxHour = 48
-      let nextWeekHour = 0
-      const nextWeekSchedules = []
-      const currentSchedules = this.inputSchedules
+      // const maxHour = 48
+      // let nextWeekHour = 0
+      // const nextWeekSchedules = []
+      // const currentSchedules = this.inputSchedules
       // console.log(currentSchedules)
 
       // cari jadwal sampai angka >= 48 jam
 
-      for (const schedule of currentSchedules) {
-        nextWeekHour += schedule.hour
-        nextWeekSchedules.push(schedule)
-        if (nextWeekHour >= maxHour) {
-          break
-        }
-      }
+      // for (const schedule of currentSchedules) {
+      //   nextWeekHour += schedule.hour
+      //   nextWeekSchedules.push(schedule)
+      //   if (nextWeekHour >= maxHour) {
+      //     break
+      //   }
+      // }
 
-      if (nextWeekHour === maxHour) {
-        this.nextWeekSchedules = nextWeekSchedules
-      } else {
-        // kurangi satu jika ada 49
-        const offsetSchedules = nextWeekSchedules.filter((e) => e.hour === 1)
-        // cari yang terakhir
-        const offsetSchedule = offsetSchedules[offsetSchedules.length - 1]
-        // console.log(nextWeekHour)
-        // console.log(offsetSchedules)
-        // console.log(offsetSchedule)
+      // if (nextWeekHour === maxHour) {
+      //   this.nextWeekSchedules = nextWeekSchedules
+      // } else {
+      //   // kurangi satu jika ada 49
+      //   const offsetSchedules = nextWeekSchedules.filter((e) => e.hour === 1)
+      //   // cari yang terakhir
+      //   const offsetSchedule = offsetSchedules[offsetSchedules.length - 1]
+      //   // console.log(nextWeekHour)
+      //   // console.log(offsetSchedules)
+      //   // console.log(offsetSchedule)
 
-        nextWeekSchedules.splice(nextWeekSchedules.indexOf(offsetSchedule, 1))
-        this.nextWeekSchedules = nextWeekSchedules
-      }
+      //   nextWeekSchedules.splice(nextWeekSchedules.indexOf(offsetSchedule, 1))
+      //   this.nextWeekSchedules = nextWeekSchedules
+      // }
 
       // console.log(this.nextWeekSchedules.length)
 
       // cari jadwal minggu tertunda
 
-      const remainingSchedules = this.inputSchedules.filter(
-        (e) => !this.nextWeekSchedules.includes(e)
-      )
-      // console.log(remainingSchedules)
-      this.nextWeekPostponedSchedules = remainingSchedules
+      // const remainingSchedules = this.inputSchedules.filter(
+      //   (e) => !this.nextWeekSchedules.includes(e)
+      // )
+      // // console.log(remainingSchedules)
+      // this.nextWeekPostponedSchedules = remainingSchedules
 
       // console.log(this.nextWeekSchedules)
       // populate table
@@ -543,22 +571,22 @@ export default {
       //     friday: schedule.days.friday ? schedule.name : '',
       //     saturday: schedule.days.saturday ? schedule.name : ''
 
-      const initTime = this.$moment('08:00', 'H:mm')
-      for (let index = 0; index < 8; index++) {
-        const dailySchedule = {
-          range: `${initTime.format('H:mm')} - ${initTime
-            .add(1, 'hours')
-            .format('H:mm')}`,
-          monday: 'Not Implemented Error',
-          tuesday: 'Not Implemented Error',
-          wednesday: 'Not Implemented Error',
-          thursday: 'Not Implemented Error',
-          friday: 'Not Implemented Error',
-          saturday: 'Not Implemented Error',
-        }
-        this.nextWeekSchedulesInTable.push(dailySchedule)
-        this.nextWeekPostponedSchedulesInTable.push(dailySchedule)
-      }
+      // const initTime = this.$moment('08:00', 'H:mm')
+      // for (let index = 0; index < 8; index++) {
+      //   const dailySchedule = {
+      //     range: `${initTime.format('H:mm')} - ${initTime
+      //       .add(1, 'hours')
+      //       .format('H:mm')}`,
+      //     monday: 'Not Implemented Error',
+      //     tuesday: 'Not Implemented Error',
+      //     wednesday: 'Not Implemented Error',
+      //     thursday: 'Not Implemented Error',
+      //     friday: 'Not Implemented Error',
+      //     saturday: 'Not Implemented Error',
+      //   }
+      //   this.nextWeekSchedulesInTable.push(dailySchedule)
+      //   this.nextWeekPostponedSchedulesInTable.push(dailySchedule)
+      // }
     },
   },
   head: {
